099535b0153d1c1a659d4e5d82b7ae9890792879
Mayur Kalia <mayurkalia96@gmail.com>
2025-09-10 17:25:25 +0530

    Comment out CompanyTimeline component in About page for future consideration

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -100,7 +100,7 @@ const About: React.FC = () => {
       <AboutUsSection imageRefs={imageRefs} counterRefs={counterRefs} />
 
       {/* Company Timeline Section */}
-      <CompanyTimeline imageRefs={imageRefs} />
+      {/* <CompanyTimeline imageRefs={imageRefs} /> */}
 
       {/* Vision Mission Section */}
       <VisionMissionSection imageRefs={imageRefs} />
